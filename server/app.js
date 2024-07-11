e3e2af83dd9b3951f10a1aab325f547d6f042518
Vijaykumar <vijaykumar2022b@gmail.com>
2024-07-11 17:48:33 +0530

    fff

--- server/app.js
+++ server/app.js
@@ -234,7 +234,7 @@ app.delete("/api/userschatbox", verifyAuthorization, deleteUsersChatboxHandler);
 
 // Start server
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
